308f180f8a8005b95cfe5850a3eb89fa0db53584
souravchand04 <souravchand1109@gmail.com>
2026-05-14 18:48:18 +0530

    resolved the class named issue

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import "./global.css"
+import "./global.css";
 import { Text, View } from "react-native";
  
 export default function App() {
---
+++ nativewind-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="nativewind/types" />
+
+// NOTE: This file should be committed with your source code so TypeScript can
+// understand NativeWind's React Native className props.
